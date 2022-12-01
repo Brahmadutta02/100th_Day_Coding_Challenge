8dc73edbb1325cb596ea684ba1b2f9a7c14a9915
Brahmadutta02 <brahmaduttadalai012@gmail.com>
2022-12-01 20:09:03 +0530

    adding Stone-Paper-Scissor Game  using React

---
+++ Day_25/src/App.js
@@ -0,0 +1,101 @@
+import React,{useState, useEffect} from 'react'
+import './App.css';
+
+function App() {
+  const [userChoice, setUserChoice] = useState('rock')
+  const [computerChoice, setComputerChoice] = useState('rock')
+  const [userPoints, setUserPoints] = useState(0)
+  const [computerPoints, setComputerPoints] = useState(0)
+  const[turnResult, setTurnResult] = useState(null)
+  const[result, setResult] = useState('Let\'s see who wins')
+  const[gameOver, setGameOver] = useState(false)
+  
+  const choices = ['rock', 'paper', 'scissors']
+
+  const handleClick = (value) => {
+    setUserChoice(value)    
+    generateComputerChoice()
+  }
+
+  const generateComputerChoice = () => {
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)]
+    setComputerChoice(randomChoice)
+  }
+
+  const reset = () => {
+    window.location.reload()
+  }
+
+  useEffect(() => {
+    const comboMoves = userChoice + computerChoice
+    if (userPoints <= 4 && computerPoints <= 4) {
+      if (comboMoves === 'scissorspaper' || comboMoves === 'rockscissors' || comboMoves === 'paperrock') {
+        // userPoints.current += 1
+        const updatedUserPoints = userPoints + 1
+        setUserPoints(updatedUserPoints)
+        setTurnResult('User gets the point!')
+        if (updatedUserPoints === 5){
+          setResult('User Wins')
+          const gameOff = true
+          setGameOver(gameOff)
+        }
+      }
+
+      if (comboMoves === 'paperscissors' || comboMoves === 'scissorsrock' || comboMoves === 'rockpaper') {
+        // computerPoints.current += 1
+        const updatedComputerPoints = computerPoints + 1
+        setComputerPoints(updatedComputerPoints)
+        setTurnResult('Computer gets the point!')
+        if (updatedComputerPoints === 5) {
+          setResult('Computer Wins')
+          const gameOff = true
+          setGameOver(gameOff)
+        }
+      }
+
+      if (comboMoves === 'paperpaper' || comboMoves === 'rockrock' || comboMoves === 'scissorsscissors') {
+        setTurnResult('No one gets a point!')
+      }
+    }
+  }, [computerChoice, userChoice])
+
+  return (
+    <div className="App">
+      <h1 className='heading'>Rock-Paper-Scissors</h1>
+      <div className='score'>
+        <h1>User Points: {userPoints}</h1>
+        <h1>Computer Points: {computerPoints}</h1>
+      </div>
+
+      <div className='choice'>
+        <div className='choice-user'>
+          <img className='user-hand' src={`../images/${userChoice}.png`} alt=''></img>
+        </div>
+        <div className='choice-computer'>
+          <img className='computer-hand' src={`../images/${computerChoice}.png`} alt=''></img>
+        </div>
+      </div>
+      
+      <div className='button-div'>
+        {choices.map((choice, index) =>
+          <button className='button' key={index} onClick={() => handleClick(choice)} disabled={gameOver}>
+            {choice} 
+          </button>
+        )}
+      </div>
+      
+      <div className='result'>
+        <h1>Turn Result: {turnResult}</h1>
+        <h1>Final Result: {result}</h1>
+      </div>
+      
+      <div className='button-div'>
+        {gameOver && 
+          <button className='button' onClick={() => reset()}>Restart Game?</button>
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App;
