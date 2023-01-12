5b6d4744ad0c9aad7f19ea9568b589e007a6a042
Brahmadutta02 <brahmaduttadalai012@gmail.com>
2023-01-12 21:40:51 +0530

    adding Numeric value incresing/decreasing Project using React

---
+++ Day_67/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import Resturant from "./component/Basics/Resturant";cd 
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return <UseReducer />;
+};
+
+export default App;
